8a1148e31d9ca515bfa4c517c2bf64778684c1f6
Ewa <pawinska.ewa@wp.pl>
2019-03-24 14:42:06 +0100

    Adding calculate() implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,23 @@
 import calculate.BMICalculator;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        BMICalculator BMIcalculator = new BMICalculator(168, 55);
+
+        int height;
+        int weight;
+
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your height in [cm]:");
+        height = sc.nextInt();
+
+        System.out.println("Enter your weight in [kg]:");
+        weight = sc.nextInt();
+
+        BMICalculator BMIcalculator = new BMICalculator(height, weight);
+        System.out.println(BMIcalculator.calculate());
+        System.out.println(BMIcalculator.interpret());
     }
 }
--- src/main/java/calculate/BMICalculator.java
+++ src/main/java/calculate/BMICalculator.java
@@ -1,20 +1,51 @@
 package calculate;
 
+
+import java.util.Arrays;
+
 public class BMICalculator implements Calculate {
 
     private int weight;
     private int height;
+    private double BMI;
+
+    public BMICalculator(int height, int weight) {
+        this.height = height;
+        this.weight = weight;
+    }
 
     @Override
     public String calculate() {
-        return "0";
+        double heightInMeters = (double)height/100;
+        double BMI = weight / Math.pow(heightInMeters, 2);
+        this.BMI = BMI;
+        return "Your BMI is: " + BMI;
     }
 
     @Override
     public String interpret() {
-        return "";
+        double[] table = new double[]{15, 16, 18.5, 25, 30, 35, 40, 45, 50, 60, (double)Integer.MAX_VALUE};
+
+        BMI = (int)(100 * BMI);
+        BMI = BMI / 100.00;
+
+        int index = Arrays.binarySearch(table,BMI);
+        System.out.println(BMI);
+        System.out.println("index is: " + index);
+        switch(index){
+            case 0: return "You are: Very severely underweight";
+            case 1: return "You are: Severely underweight";
+            case 2: return "You are: Underweight";
+            case 3: return "You are: Normal (healthy weight)";
+            case 4: return "You are: Overweight";
+            case 5: return "You are: Obese Class I (Moderately obese)";
+            case 6: return "You are: Obese Class II (Severely obese)";
+            case 7: return "You are: Obese Class III (Very severely obese)";
+            case 8: return "You are: Obese Class IV (Morbidly Obese)";
+            case 9: return "You are: Obese Class V (Super Obese)";
+            case 10: return "You are: Obese Class VI (Hyper Obese)";
+            default: return "Something went wrong";
+        }
     }
 
-    public BMICalculator(int height, int weight) {
-    }
 }
